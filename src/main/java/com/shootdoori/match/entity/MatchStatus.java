5730f4cfa25e77f204ffe40d65e618ed911ddaa4
WonJuN Sun <swj010324@naver.com>
2025-09-05 14:55:02 +0900

    feat(entity) : 팀유형, 수준, 매치 요청, 대기, 확정에 대한 enum class 정의

---
+++ src/main/java/com/shootdoori/match/entity/MatchStatus.java
@@ -0,0 +1,8 @@
+package com.shootdoori.match.entity;
+
+public enum MatchStatus {
+  모집중,
+  매칭완료,
+  경기완료,
+  취소
+}
---
+++ src/main/java/com/shootdoori/match/entity/MatchWaitingStatus.java
@@ -0,0 +1,8 @@
+package com.shootdoori.match.entity;
+
+public enum MatchWaitingStatus {
+  대기중,
+  완료,
+  취소,
+  만료
+}
