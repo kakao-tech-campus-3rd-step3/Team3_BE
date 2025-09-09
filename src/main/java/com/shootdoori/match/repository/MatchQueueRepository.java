9453070a1e6db9b43f06a774c100c4f28161d7fe
WonJuN Sun <swj010324@naver.com>
2025-09-09 15:47:42 +0900

    refactor(repository) : entity 명에 맞게 파일명 수정, status 값 비교시 enum 활용토록 수정, expireAt 관련한 조건 추가, offset 기반 페이징에서 커서 기반 페이징으로 수정

--- src/main/java/com/shootdoori/match/repository/MatchQueueRepository.java
+++ src/main/java/com/shootdoori/match/repository/MatchQueueRepository.java
@@ -2,24 +2,29 @@ package com.shootdoori.match.repository;
 
 import com.shootdoori.match.entity.MatchQueue;
 import java.time.LocalDate;
-import org.springframework.data.domain.Page;
+import java.time.LocalTime;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MatchWaitingRepository extends JpaRepository<MatchQueue, Long> {
+public interface MatchQueueRepository extends JpaRepository<MatchQueue, Long> {
 
-    @Query("SELECT mw FROM MatchWaiting mw " +
-        "WHERE mw.preferredDate = :date " +
-        "AND mw.status = '대기중' " +
-        "AND mw.team.id <> :teamId " + //우리 팀 제외
-        "ORDER BY mw.preferredTimeStart ASC")
-    Page<MatchQueue> findAvailableMatchesByDate(
+    @Query("SELECT mq FROM MatchQueue mq " +
+        "WHERE mq.preferredDate = :date " +
+        "AND mq.status = com.shootdoori.match.entity.MatchQueueStatus.WAITING " +
+        "AND mq.team.id <> :teamId " +
+        "AND mq.expiresAt > CURRENT_TIMESTAMP " +
+        "AND (:lastTime IS NULL OR mq.preferredTimeStart > :lastTime) " +
+        "ORDER BY mq.preferredTimeStart ASC")
+    Slice<MatchQueue> findAvailableMatchesByDateCursor(
         @Param("date") LocalDate date,
         @Param("teamId") Long teamId,
-        Pageable pageable);
+        @Param("lastTime") LocalTime lastTime,
+        Pageable pageable
+    );
 }
 
