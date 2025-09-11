4b49f36c8003d219a95186c0673dd9c8f2e1973d
KKPASII <kkongpasi1005@gmail.com>
2025-09-12 01:08:03 +0900

    feat(mercenary): 용병 모집 CRUD API 구현

---
+++ src/main/java/com/shootdoori/match/service/MercenaryRecruitmentService.java
@@ -0,0 +1,82 @@
+package com.shootdoori.match.service;
+
+import com.shootdoori.match.dto.RecruitmentCreateRequest;
+import com.shootdoori.match.dto.RecruitmentResponse;
+import com.shootdoori.match.dto.RecruitmentUpdateRequest;
+import com.shootdoori.match.entity.MercenaryRecruitment;
+import com.shootdoori.match.entity.Position;
+import com.shootdoori.match.entity.SkillLevel;
+import com.shootdoori.match.entity.Team;
+import com.shootdoori.match.exception.TeamNotFoundException;
+import com.shootdoori.match.repository.MercenaryRecruitmentRepository;
+import com.shootdoori.match.repository.TeamRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MercenaryRecruitmentService {
+    private final MercenaryRecruitmentRepository recruitmentRepository;
+    private final TeamRepository teamRepository;
+
+    public MercenaryRecruitmentService(MercenaryRecruitmentRepository recruitmentRepository, TeamRepository teamRepository) {
+        this.recruitmentRepository = recruitmentRepository;
+        this.teamRepository = teamRepository;
+    }
+
+    public RecruitmentResponse create(RecruitmentCreateRequest request) {
+        // TODO: 요청을 보낸 사용자가 이 게시글을 생성할 권한이 있는지 확인하는 로직 추가
+
+        Team team = teamRepository.findById(request.teamId()).orElseThrow(
+            () -> new TeamNotFoundException("해당 팀을 찾을 수 없습니다. id = " + request.teamId()));
+
+        Position position = Position.fromDisplayName(request.position());
+        SkillLevel skillLevel = SkillLevel.fromDisplayName(request.skillLevel());
+
+        MercenaryRecruitment savedRecruitment = recruitmentRepository.save(MercenaryRecruitment.create(
+            team, request.matchDate(), request.matchTime(), request.message(), position, skillLevel));
+
+        return new RecruitmentResponse(savedRecruitment);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<RecruitmentResponse> findAllPages(Pageable pageable) {
+        Page<MercenaryRecruitment> recruitments = recruitmentRepository.findAll(pageable);
+
+        return recruitments.map(RecruitmentResponse::new);
+    }
+
+    @Transactional(readOnly = true)
+    public RecruitmentResponse findById(Long id) {
+        MercenaryRecruitment recruitment = recruitmentRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 모집 공고입니다."));
+
+        return new RecruitmentResponse(recruitment);
+    }
+
+    public RecruitmentResponse update(Long id, RecruitmentUpdateRequest updateRequest) {
+        // TODO: 요청을 보낸 사용자가 이 게시글을 수정할 권한이 있는지 확인하는 로직 추가
+
+        MercenaryRecruitment recruitment = recruitmentRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 모집 공고입니다."));
+
+        Position position = Position.fromDisplayName(updateRequest.position());
+        SkillLevel skillLevel = SkillLevel.fromDisplayName(updateRequest.skillLevel());
+
+        recruitment.updateRecruitmentInfo(
+            updateRequest.matchDate(), updateRequest.matchTime(), updateRequest.message(), position, skillLevel);
+
+        return new RecruitmentResponse(recruitment);
+    }
+
+    public void delete(Long id) {
+        // TODO: 요청을 보낸 사용자가 이 게시글을 삭제할 권한이 있는지 확인하는 로직 추가
+
+        recruitmentRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 모집 공고입니다."));
+
+        recruitmentRepository.deleteById(id);
+    }
+}
