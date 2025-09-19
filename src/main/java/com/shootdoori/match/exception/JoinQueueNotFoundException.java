27f2db895c3c4cc3678ceb1a1fdc868f2c60fda3
raminKim <151427064+raminkim@users.noreply.github.com>
2025-09-20 00:31:56 +0900

    refactor: JoinQueueNotFoundException에 404 반환하도록 수정

--- src/main/java/com/shootdoori/match/exception/JoinQueueNotFoundException.java
+++ src/main/java/com/shootdoori/match/exception/JoinQueueNotFoundException.java
@@ -1,5 +1,9 @@
 package com.shootdoori.match.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class JoinQueueNotFoundException extends BusinessException {
   public JoinQueueNotFoundException() {
     super(ErrorCode.JOIN_QUEUE_NOT_FOUND);
