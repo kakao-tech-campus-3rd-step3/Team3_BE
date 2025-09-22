c041d22612813822f18346602915c1491398cafc
KKPASII <kkongpasi1005@gmail.com>
2025-09-22 23:54:02 +0900

    feat(profile): 커스텀 예외 클래스 추가 및 적용

--- src/main/java/com/shootdoori/match/exception/ErrorCode.java
+++ src/main/java/com/shootdoori/match/exception/ErrorCode.java
@@ -19,7 +19,9 @@ public enum ErrorCode {
 
     NO_PERMISSION("허락되지 않은 요청입니다."),
 
-    RECRUITMENT_NOT_FOUND("존재하지 않는 모집 공고입니다.");
+    RECRUITMENT_NOT_FOUND("존재하지 않는 모집 공고입니다."),
+
+    PROFILE_NOT_FOUND("해당 프로필을 찾을 수 없습니다.");
 
     private final String message;
 
