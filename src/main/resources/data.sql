ded434437e3843fa7add99e4fd3891ffda8f5492
raminKim <151427064+raminkim@users.noreply.github.com>
2025-09-22 16:49:36 +0900

    chore: application.properties 환경 및 매치 생성을 위한 data.sql 세팅

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO venues(venue_name, address, latitude, longitude, contact_info, facilities, price_per_hour)
+VALUES ('강원대학교 체육관', '강원도 춘천시 강원대학길1', 37.8694, 127.7451, '033-123-1234', '샤워실, 라커룸 완비', 10000);
