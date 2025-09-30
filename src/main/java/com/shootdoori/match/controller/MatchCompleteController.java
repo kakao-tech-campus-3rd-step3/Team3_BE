56251aa1e0e51a289bf42f7fadd4f3045b49ca01
ManDu2001 <swj010324@naver.com>
2025-10-01 03:38:35 +0900

    refactor(match_start) : MatchCompleteController 에@LoginUser 적용 및 이에 따른 Service, Dto 변경

--- src/main/java/com/shootdoori/match/controller/MatchCompleteController.java
+++ src/main/java/com/shootdoori/match/controller/MatchCompleteController.java
@@ -1,6 +1,7 @@
 package com.shootdoori.match.controller;
 import com.shootdoori.match.dto.MatchTeamRequestDto;
 import com.shootdoori.match.dto.TeamResponseDto;
+import com.shootdoori.match.resolver.LoginUser;
 import com.shootdoori.match.service.MatchCompleteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +19,9 @@ public class MatchCompleteController {
         this.matchCompleteService = matchCompleteService;
     }
 
-
-    /**
-     * 특정 매치의 상대팀 정보 조회
-     * @param matchTeamDto 매치 id, 내 팀 id
-     * @return TeamResponseDto JSON
-     */
     @GetMapping("/enemyTeam")
-    public ResponseEntity<TeamResponseDto> getEnemyTeam(@RequestBody MatchTeamRequestDto matchTeamDto) {
-        return new ResponseEntity<>(matchCompleteService.getEnemyTeam(matchTeamDto), HttpStatus.OK);
+    public ResponseEntity<TeamResponseDto> getEnemyTeam(@LoginUser Long loginUserId,
+                                                        @RequestBody MatchTeamRequestDto matchTeamDto) {
+        return new ResponseEntity<>(matchCompleteService.getEnemyTeam(loginUserId, matchTeamDto), HttpStatus.OK);
     }
 }
