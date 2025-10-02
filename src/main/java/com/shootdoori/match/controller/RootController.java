317686efdd9365eedbdcf435d23190b212a49ae4
raminKim <151427064+raminkim@users.noreply.github.com>
2025-10-03 02:48:33 +0900

    feat(main): 앱 소개 페이지 추가

--- src/main/java/com/shootdoori/match/controller/RootController.java
+++ src/main/java/com/shootdoori/match/controller/RootController.java
@@ -7,11 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 
-    @GetMapping("/")
-    public ResponseEntity<String> root() {
-        return ResponseEntity.ok("OK");
-    }
-
     @GetMapping("/health")
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK");
