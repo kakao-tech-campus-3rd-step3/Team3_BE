aa366a1ed6a8d9327ad7ac053e3381936ef3bc4b
ManDu2001 <swj010324@naver.com>
2025-10-14 15:13:09 +0900

    refactor(repository) : 매치 수락시 다른 요청을 거절할 때, 쿼리가 아닌 Service에서 객체 상태 변경하도록 수정

--- src/main/java/com/shootdoori/match/repository/MatchRequestRepository.java
+++ src/main/java/com/shootdoori/match/repository/MatchRequestRepository.java
@@ -9,18 +9,18 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface MatchRequestRepository extends JpaRepository<MatchRequest, Long> {
 
-    @Modifying(clearAutomatically = true)
-    @Query("UPDATE MatchRequest mr " +
-        "SET mr.status = com.shootdoori.match.entity.match.request.MatchRequestStatus.REJECTED " +
+    @Query("SELECT mr FROM MatchRequest mr " +
         "WHERE mr.targetTeam.teamId = :targetTeamId " +
         "AND mr.status = com.shootdoori.match.entity.match.request.MatchRequestStatus.PENDING " +
         "AND mr.requestId <> :acceptedRequestId " +
-        "AND mr.matchWaiting.waitingId = :waitingId ")
-    int rejectOtherRequests(@Param("targetTeamId") Long targetTeamId,
-                            @Param("acceptedRequestId") Long acceptedRequestId,
-                            @Param("waitingId") Long waitingId);
+        "AND mr.matchWaiting.waitingId = :waitingId")
+    List<MatchRequest> findRequestsToReject(@Param("targetTeamId") Long targetTeamId,
+                                            @Param("acceptedRequestId") Long acceptedRequestId,
+                                            @Param("waitingId") Long waitingId);
 
     @Query("SELECT mr FROM MatchRequest mr " +
         "WHERE mr.targetTeam.teamId = :targetTeamId " +
