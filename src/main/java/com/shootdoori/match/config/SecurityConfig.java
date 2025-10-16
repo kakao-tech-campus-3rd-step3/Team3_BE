3a455e41a59537e790b80b821cee3f3b1326209d
raminKim <151427064+raminkim@users.noreply.github.com>
2025-10-16 22:44:25 +0900

    feat: 유저 삭제 페이지 추가

--- src/main/java/com/shootdoori/match/config/SecurityConfig.java
+++ src/main/java/com/shootdoori/match/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
             .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
 
-                    .requestMatchers("/", "/health").permitAll()
+                    .requestMatchers("/", "/health", "/delete", "/login").permitAll()
                     .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/refresh").permitAll()
                     .requestMatchers("/h2-console/**").permitAll()
 
