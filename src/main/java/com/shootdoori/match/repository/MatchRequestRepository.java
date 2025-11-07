2a1d8791d5f84ce60ef978ad842c50a45e267ad7
라민 <151427064+raminkim@users.noreply.github.com>
2025-11-08 00:11:38 +0900

    fix: 매칭 신청 조회 & 매칭 생성 조회 & 가입 신청 조회 오류 수정, 테스트 삭제 (#205)
    * fix: 매칭 신청 조회 오류 수정
    
    * test: 필요없는 테스트 삭제

--- src/main/java/com/shootdoori/match/repository/MatchRequestRepository.java
+++ src/main/java/com/shootdoori/match/repository/MatchRequestRepository.java
@@ -28,12 +28,14 @@ public interface MatchRequestRepository extends JpaRepository<MatchRequest, Long
         "ORDER BY mr.requestAt ASC ")
     Slice<MatchRequest> findPendingRequestsByTargetTeam(@Param("targetTeamId") Long targetTeamId, Pageable pageable);
 
-    @Query("SELECT mr " +
-        "FROM MatchRequest mr " +
-        "JOIN mr.requestTeam requestTeam " +
-        "JOIN mr.targetTeam targetTeam " +
-        "WHERE requestTeam.teamId = :requestTeamId " +
-        "ORDER BY mr.requestAt DESC ")
+    @Query("""
+        SELECT mr
+        FROM MatchRequest mr
+        JOIN FETCH mr.requestTeam requestTeam
+        JOIN FETCH mr.targetTeam targetTeam
+        WHERE requestTeam.teamId = :requestTeamId
+        ORDER BY mr.requestAt DESC
+    """)
     Slice<MatchRequest> findSentRequestsByTeam(@Param("requestTeamId") Long requestTeamId,
                                                Pageable pageable);
 
